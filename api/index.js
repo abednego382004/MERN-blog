659a95b26d3806211ef63a6d5f3e1695bdfc5c4e
abed <abed382004@gmail.com>
2025-03-16 09:19:45 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(5000, () => {
   console.log("api is running on port 5000");
