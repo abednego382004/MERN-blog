35e2c0af42f0358ae7249604800406de97af43ee
abed <abed382004@gmail.com>
2025-03-18 09:29:29 +0530

    complete side dash and profile

---
+++ client/src/component/DashSidebar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
+export default function DashSidebar() {
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="dashboard?tab=profile">
+            <Sidebar.Item active icon={HiUser} label={"User"} labelColor="dark">
+              Profile
+            </Sidebar.Item>
+            <Sidebar.Item
+              active
+              icon={HiArrowSmRight}
+              className="cursor-pointer"
+            >
+              Sign Up
+            </Sidebar.Item>
+          </Link>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
--- client/src/component/PrivateRoute.jsx
+++ client/src/component/PrivateRoute.jsx
@@ -4,7 +4,6 @@ import { Outlet, Navigate } from "react-router-dom";
 
 export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser);
 
   return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
