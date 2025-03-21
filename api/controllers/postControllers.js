fa951a202034641874b89f4ac06d66ef4d7d6c27
abed <abed382004@gmail.com>
2025-03-21 17:43:08 +0530

    create and get post api route

--- api/controllers/postControllers.js
+++ api/controllers/postControllers.js
@@ -49,3 +49,51 @@ export const imageUpload = async (req, res, next) => {
     res.status(200).json({ imageUrl: req.file.path });
   });
 };
+
+export const getPost = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex || 0);
+    const limit = parseInt(req.query.limit || 9);
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+    const filters = {
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchItem && {
+        $or: [
+          { title: { $regex: req.query.searchItem, $options: "i" } },
+          { content: { $regex: req.query.searchItem, $options: "i" } },
+        ],
+      }),
+    };
+
+    const posts = await Post.find(filters)
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create, imageUpload } from "../controllers/postControllers.js";
+import {
+  create,
+  getPost,
+  imageUpload,
+} from "../controllers/postControllers.js";
 
 const postRouter = express.Router();
 
 postRouter.post("/create", verifyToken, create);
 postRouter.post("/upload", imageUpload);
+postRouter.get("/getposts", getPost);
 
 export default postRouter;
