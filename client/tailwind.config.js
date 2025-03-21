e501de21ae3edee687f99fa1e6495cce506a4ba8
abed <abed382004@gmail.com>
2025-03-21 20:23:21 +0530

    show user post inside dashpost

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// const flowbite = require("flowbite-react/tailwind");
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [flowbite.plugin()],
+// };
+
 /** @type {import('tailwindcss').Config} */
 const flowbite = require("flowbite-react/tailwind");
+const tailwindScrollbar = require("tailwind-scrollbar"); // ✅ Import the plugin
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin()],
+  plugins: [
+    flowbite.plugin(),
+    tailwindScrollbar, // ✅ Add the scrollbar plugin
+  ],
 };
