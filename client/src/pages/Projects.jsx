d769819376498f69f1c430ade2bd9fb4abff72c2
abed <abed382004@gmail.com>
2025-03-26 13:57:26 +0530

    complete project and about pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import CallToAcrion from "../component/CallToAcrion";
 
-const Projects = () => {
-  return <div>Projects</div>;
-};
-
-export default Projects;
+export default function Projects() {
+  return (
+    <div className="min-h-screen w-full mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning HTML, CSS, and
+        JavaScript!
+      </p>
+      <CallToAcrion />
+    </div>
+  );
+}
